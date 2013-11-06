c50d801e06cd339c0aef55c60ad6f9ca5131949e
crackdog <flo.kahllund@googlemail.com>
2013-11-06 01:38:03 +0100

    adding first public function prototypes

--- Connection.java
+++ Connection.java
@@ -1,11 +1,27 @@
 
-public class Connection {
-  public Connection() {
-    System.out.println("---constructor---");
-  }
-  
-  public void print() {
-    System.out.println("Hello World.");
-  }
-}
+import java.net.Socket;
 
+public class Connection {
+
+    private Socket server;
+    Encryption crypt = new Encryption();
+
+    public Connection() {
+        System.out.println("---Connection constructor---");
+    }
+
+    public boolean sendToServer(String msg) {
+        msg = crypt.encryptMsg(msg);
+        
+        System.out.println("send encrypted msg: '" + msg + "'");
+        
+        return false;
+    }
+
+    public String recvFromServer() {
+        String msg = "";
+        
+        msg = crypt.decryptMsg(msg);
+        return msg;
+    }
+}
