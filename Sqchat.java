109a4b5f0a0e761575d9ace57a00b7b07c195809
crackdog <flo.kahllund@googlemail.com>
2013-11-06 01:38:56 +0100

    formatted

--- Sqchat.java
+++ Sqchat.java
@@ -1,9 +1,9 @@
 
 public class Sqchat {
-  public static void main(String args[]) {
-    System.out.println("Hello World!");
-    Connection test = new Connection();
-    test.print();
-  }
-}
 
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+        Connection test = new Connection();
+        test.sendToServer("Hello World!");
+    }
+}
