f9fa73831d5f2576b4d68e1cd9d338d3ba591aa2
crackdog <flo.kahllund@googlemail.com>
2013-11-06 01:37:18 +0100

    adding public function prototypes

--- Encryption.java
+++ Encryption.java
@@ -1,7 +1,14 @@
 
 public class Encryption {
-  public Encryption() {
-    
-  }
-}
 
+    public Encryption() {
+    }
+    
+    public String decryptMsg(String msg) {
+        return msg;
+    }
+    
+    public String encryptMsg(String msg) {
+        return msg;
+    }
+}
