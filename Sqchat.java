8f61962eb8a7cd9cf197f1ab5756768960ecccc9
crackdog <flo.kahllund@googlemail.com>
2013-11-07 01:01:03 +0100

    testing Encryption.java

--- Sqchat.java
+++ Sqchat.java
@@ -4,7 +4,9 @@ public class Sqchat {
 
     public static void main(String args[]) {
         System.out.println("Hello World!");
-        Connection test = new Connection();
-        test.sendToServer("Hello World!");
+        Encryption crypt = new Encryption("R2pjY2AvWGB9Y2su");
+        String test = crypt.encryptMsg("Hello World!");
+        System.out.println(test);
+        System.out.println(crypt.decryptMsg(test));
     }
 }
