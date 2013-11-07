fe065fc7bca645c188501341b046f40008d6ef81
crackdog <flo.kahllund@googlemail.com>
2013-11-07 23:34:56 +0100

    implementing base64decode

--- sqchat/Encryption.java
+++ sqchat/Encryption.java
@@ -16,24 +16,21 @@ public class Encryption {
     }
 
     public Encryption(String base64key) {
-        key = decode(base64key).getBytes();
+        key = base64decode(base64key);
         keylen = key.length;
     }
 
     public String decryptMsg(String msg) {
-        return xor_crypt(decode(msg));
+        return new String(base64decode(msg));
     }
 
     public String encryptMsg(String msg) {
         return encode(xor_crypt(msg));
     }
 
-    private String xor_crypt(String s) {
-        byte bytes[];
+    private byte[] xor_crypt(byte[] bytes) {
         int i, j;
-        String ret;
 
-        bytes = s.getBytes();
         i = 0;
         j = 0;
 
@@ -48,16 +45,52 @@ public class Encryption {
             }
         }
 
-        ret = new String(bytes);
-
-        return ret;
+        return bytes;
     }
 
-    private String encode(String s) {
+    private String xor_crypt(String s) {
+        return new String(xor_crypt(s.getBytes()));
+    }
+
+    public String base64encode(byte[] rawdata) {
+        String base64str, padStr;
+        int padCount, datalength, i, tmp;
+        byte[] data;
+        
+        datalength = rawdata.length + (rawdata.length % 3);
+        data = new byte[datalength];
+        
+        for(i = 0; i < datalength; i++) {
+            if(i < rawdata.length) {
+                data[i] = rawdata[i];
+            } else {
+                data[i] = '\0';
+            }
+        }
+        
+        base64str = "";
+        padStr = "";
+        
+        padCount = rawdata.length % 3;
+        if(padCount > 0) {
+            while(padCount < 3) {
+                padStr += '=';
+                padCount++;
+            }
+        }
+        
+        for(i = 0; i < data.length; i+=3) {
+            
+        }
+        
+        return base64str;
+    }
+
+    public String encode(String s) {
 
         //I copied the code from 
         //https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64#Java
-        
+
         // the result/encoded string, the padding string, and the pad count
         String r = "", p = "";
         int c = s.length() % 3;
@@ -94,8 +127,48 @@ public class Encryption {
         return r.substring(0, r.length() - p.length()) + p;
     }
 
-    private String decode(String s) {
-        
+    private byte[] base64decode(String encodedDataString) {
+        int decodedDataLength, encodedDataLength;
+        byte[] decodedData, encodedData;
+        int padCount, i, j, tmp;
+
+        //remove all characters that are not in the base64 character list
+        encodedDataString = encodedDataString.replaceAll("[^" + base64chars + "=]", "");
+
+        //saving encodedDataString as byte Array
+        encodedDataLength = encodedDataString.length();
+        encodedData = encodedDataString.getBytes();
+
+        //replace padding chars with a pad that decodes to zero ("A")
+        if (encodedData[encodedDataLength - 1] == '=') {
+            encodedData[encodedDataLength - 1] = 'A';
+
+            if (encodedData[encodedDataLength - 2] == '=') {
+                encodedData[encodedDataLength - 2] = 'A';
+            }
+        }
+
+        decodedDataLength = encodedDataLength / 4 * 3 + 1;
+        decodedData = new byte[decodedDataLength];
+
+        for (i = 0, j = 0; i < encodedDataLength && j < decodedDataLength; i += 4, j += 3) {
+            //read 4 numbers and store in 24 bit number
+            tmp = base64chars.indexOf(encodedData[i]) << 18;
+            tmp += base64chars.indexOf(encodedData[i + 1]) << 12;
+            tmp += base64chars.indexOf(encodedData[i + 2]) << 6;
+            tmp += base64chars.indexOf(encodedData[i + 3]);
+
+            //save 24 bit number in 3 bytes...
+            decodedData[j] = (byte) ((tmp >> 16) & 0xff);
+            decodedData[j + 1] = (byte) ((tmp >> 8) & 0xff);
+            decodedData[j + 2] = (byte) (tmp & 0xff);
+        }
+
+        return decodedData;
+    }
+
+    public String decode(String s) {
+
         //I copied the code from 
         //https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64#Java
 
