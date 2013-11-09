e82b58358742283c2bfb5de1714cc9df0a3ec358
crackdog <flo.kahllund@googlemail.com>
2013-11-09 01:15:10 +0100

    deleting old function and testing new ones

--- sqchat/Encryption.java
+++ sqchat/Encryption.java
@@ -16,29 +16,24 @@ public class Encryption {
     }
 
     public Encryption(String base64key) {
-        key = decode(base64key).getBytes();
+        key = base64decode(base64key);
         keylen = key.length;
-        
-        keylen = 32;
-        key = new byte[keylen];
-
-        for (int i = 0; i < keylen; i++) {
-            key[i] = (byte) 0xf;
-        }
     }
 
     public String decryptMsg(String msg) {
-        return xor_crypt(decode(msg));
+        System.out.println("decryptl: " + msg.length());
+        return xor_crypt(base64decodeMsg(msg));
     }
 
     public String encryptMsg(String msg) {
+        System.out.println("encryptl: " + msg.length());
         return base64encode(xor_crypt(msg).getBytes());
     }
-    
+
     public String base64decodeMsg(String msg) {
         return new String(base64decode(msg));
     }
-    
+
     public String base64encodeMsg(String msg) {
         return base64encode(msg.getBytes());
     }
@@ -50,12 +45,15 @@ public class Encryption {
         j = 0;
 
         while (i < bytes.length) {
+            if(j >= key.length) {
+                System.out.println(j + " ... " + key.length + " --- " + keylen);
+                j = 0;
+            }
             bytes[i] ^= key[j];
 
             i++;
-            if (j < keylen) {
-                j++;
-            } else {
+            j++;
+            if (j >= keylen) {
                 j = 0;
             }
         }
@@ -151,43 +149,5 @@ public class Encryption {
         }
 
         return decodedData;
-    }
-
-    private String decode(String s) {
-
-        //I copied the code from 
-        //https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64#Java
-
-        // remove/ignore any characters not in the base64 characters list
-        // or the pad character -- particularly newlines
-        s = s.replaceAll("[^" + base64chars + "=]", "");
-
-        // replace any incoming padding with a zero pad (the 'A' character is
-        // zero)
-        String p = (s.charAt(s.length() - 1) == '='
-                ? (s.charAt(s.length() - 2) == '=' ? "AA" : "A") : "");
-        String r = "";
-        s = s.substring(0, s.length() - p.length()) + p;
-
-        // increment over the length of this encrypted string, four characters
-        // at a time
-        for (int c = 0; c < s.length(); c += 4) {
-
-            // each of these four characters represents a 6-bit index in the
-            // base64 characters list which, when concatenated, will give the
-            // 24-bit number for the original 3 characters
-            int n = (base64chars.indexOf(s.charAt(c)) << 18)
-                    + (base64chars.indexOf(s.charAt(c + 1)) << 12)
-                    + (base64chars.indexOf(s.charAt(c + 2)) << 6)
-                    + base64chars.indexOf(s.charAt(c + 3));
-
-            // split the 24-bit number into the original three 8-bit (ASCII)
-            // characters
-            r += "" + (char) ((n >>> 16) & 0xFF) + (char) ((n >>> 8) & 0xFF)
-                    + (char) (n & 0xFF);
-        }
-
-        // remove any zero pad that was added to make this a multiple of 24 bits
-        return r.substring(0, r.length() - p.length());
     }
 }
