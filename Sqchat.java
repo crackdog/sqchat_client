c6702e4a082dd0212d2c11bb8cc6f5d368418f75
crackdog <flo.kahllund@googlemail.com>
2013-11-09 01:14:22 +0100

    testing Connection.java

--- Sqchat.java
+++ Sqchat.java
@@ -4,15 +4,13 @@ public class Sqchat {
 
     public static void main(String args[]) {
         //System.out.println("Hello World!");
-        Encryption crypt = new Encryption("R2pjY2AvWGB9Y2su");
+        Encryption crypt = new Encryption("ZmphZ2hlaWF1aGdhZnVlcXdxb3FwaHY=");
+        //System.out.println(crypt.base64encodeMsg("fjagheiauhgafueqwqoqphv"));
         String test = crypt.encryptMsg("Hello World!");
         //System.out.println(test);
         //System.out.println(crypt.decryptMsg(test));
         
-        test = "Hello World!!!";
-        System.out.println(crypt.base64encodeMsg(test));
-        System.out.println(crypt.base64decodeMsg(crypt.base64encodeMsg(test)));
-        
+        test = "Hello World!!!!!";        
         System.out.println(test);
         test = crypt.encryptMsg(test);
         System.out.println(test);
