7bc774d3c38fd08a7b40bb2ca705e639abfe9050
crackdog <flo.kahllund@googlemail.com>
2013-11-12 18:14:02 +0100

    deleting old tests and adding Connection variable

--- Sqchat.java
+++ Sqchat.java
@@ -5,30 +5,14 @@ public class Sqchat {
     public static void main(String args[]) {
         
         int i;
-        //System.out.println("Hello World!");
         Encryption crypt = new Encryption("ZmphZ2hlaWF1aGdhZnVlcXdxb3FwaHY=");
-        //System.out.println(crypt.base64encodeMsg("fjagheiauhgafueqwqoqphv"));
-        String test = crypt.encryptMsg("Hello World!");
-        //System.out.println(test);
-        //System.out.println(crypt.decryptMsg(test));
+        Connection con = new Connection();
         
-        test = "Hello World!";
-        System.out.println(test);
-        test = crypt.encryptMsg(test);
+        
+        con.setEncryption(crypt);
+        
+        String test = crypt.encryptMsg("Hello World!");
         System.out.println(test);
         System.out.println(crypt.decryptMsg(test));
-        
-        test = ".";
-        for(i = 0; i < 100; i++) {
-            if(test.compareToIgnoreCase(crypt.decryptMsg(crypt.encryptMsg(test))) != 0) {
-                System.err.println("fail at: " + test);
-            }
-            test += String.valueOf((int) (Math.random() * 10));
-        }
-        if(i == 100) {
-            System.out.println("+++");
-            System.out.println(test);
-            System.out.println(crypt.encryptMsg(test));
-        }
     }
 }
