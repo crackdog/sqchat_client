9cf8aefee25c68bda9b3fa52be55bdcbfd45e59b
crackdog <flo.kahllund@googlemail.com>
2013-11-12 18:21:18 +0100

    testing Connection functions

--- Sqchat.java
+++ Sqchat.java
@@ -1,18 +1,24 @@
+
 import sqchat.*;
 
 public class Sqchat {
 
     public static void main(String args[]) {
-        
+
         int i;
         Encryption crypt = new Encryption("ZmphZ2hlaWF1aGdhZnVlcXdxb3FwaHY=");
         Connection con = new Connection();
-        
-        
+
+        con.sendToServer("Hello World!");
         con.setEncryption(crypt);
-        
-        String test = crypt.encryptMsg("Hello World!");
+        con.sendToServer("Hello World!");
+
+        for (i = 0; i < args.length; i++) {
+            con.sendToServer(args[i]);
+        }
+
+        /*String test = crypt.encryptMsg("Hello World!");
         System.out.println(test);
-        System.out.println(crypt.decryptMsg(test));
+        System.out.println(crypt.decryptMsg(test));*/
     }
 }
