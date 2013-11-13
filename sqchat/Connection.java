68b6bbbfecac46845a068e7136ba714aa2cb6610
crackdog <flo.kahllund@googlemail.com>
2013-11-13 23:19:03 +0100

    adding Socket Connection

--- sqchat/Connection.java
+++ sqchat/Connection.java
@@ -1,13 +1,35 @@
 package sqchat;
 
+import java.io.*;
 import java.net.Socket;
 
 public class Connection {
 
-    private Socket server;
-    Encryption crypt;
+    private final int stdPort = 6666;
+    private String serverIp;
+    private int serverPort;
+    private Socket serverSocket;
+    private Encryption crypt;
 
     public Connection() {
+    }
+
+    public boolean connect(String ip) {
+        return connect(ip, stdPort);
+    }
+
+    public boolean connect(String ip, int port) {
+        serverIp = ip;
+        serverPort = port;
+
+        try {
+            serverSocket = new Socket(ip, port);
+        } catch (IOException e) {
+            System.err.println(e);
+            return false;
+        }
+
+        return true;
     }
 
     public void setEncryption(Encryption c) {
@@ -17,6 +39,20 @@ public class Connection {
     public boolean sendToServer(String msg) {
         if (crypt != null) {
             msg = crypt.encryptMsg(msg);
+        }
+
+        if (serverSocket != null) {
+            try {
+                PrintWriter serverWriter =
+                        new PrintWriter(
+                        new OutputStreamWriter(
+                        serverSocket.getOutputStream()));
+
+                serverWriter.print(msg);
+                serverWriter.flush();
+            } catch (IOException e) {
+                System.err.println("no connection to server...");
+            }
         }
 
         System.out.println("send encrypted msg: '" + msg + "'");
