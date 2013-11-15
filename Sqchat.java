155717ed1e0ce81bcfa41c01d4dc31709e715ee2
crackdog <flo.kahllund@googlemail.com>
2013-11-15 17:57:42 +0100

    testing changes in Connection.java

--- Sqchat.java
+++ Sqchat.java
@@ -9,13 +9,21 @@ public class Sqchat {
         Encryption crypt = new Encryption("ZmphZ2hlaWF1aGdhZnVlcXdxb3FwaHY=");
         Connection con = new Connection();
 
+        con.setEncryption(crypt);
+        System.out.println(con.connect("localhost", 7000));
+
         con.sendToServer("Hello World!");
+        System.out.println(con.recvFromServer());
+
+        con.close();
+
+        /*con.sendToServer("Hello World!");
         con.setEncryption(crypt);
         con.sendToServer("Hello World!");
-
+        
         for (i = 0; i < args.length; i++) {
-            con.sendToServer(args[i]);
-        }
+        con.sendToServer(args[i]);
+        }*/
 
         /*String test = crypt.encryptMsg("Hello World!");
         System.out.println(test);
