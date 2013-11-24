0e84b7e8070243353c4ac754a751f7624e991c59
Florian Kahllund <flo.kahllund@googlemail.com>
2013-11-24 15:43:07 +0100

    first parts of the implementation

---
+++ gui/Frame.java
@@ -0,0 +1,13 @@
+package gui;
+
+import sqchat.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    
+    public Frame() {
+        System.out.println("Building Frame...");
+    }
+}
+ 
