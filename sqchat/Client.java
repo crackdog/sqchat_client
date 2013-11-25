002b5872f1fc0dafe7a6f7cee5f8d1f28f7276ed
Florian Kahllund <flo.kahllund@googlemail.com>
2013-11-25 11:01:30 +0100

    adding first parts of implementation of Client.java

--- sqchat/Client.java
+++ sqchat/Client.java
@@ -1,11 +1,29 @@
 package sqchat;
 
 import sqchat.*;
+import java.util.*;
 
 public class Client {
 
+    private ArrayList<Contact> contacts;
+
     public Client() {
-        System.out.println("client created");
+        contacts = new ArrayList<Contact>();
+        
+        contacts.add(new Contact());
+        contacts.add(new Contact());
+    }
+    
+    public void printContacts() {
+        for(Contact x: contacts) {
+            System.out.println(x.print());
+        }
+    }
+
+    public void importClientList(String msg) {
+        /* import a message with a clientlist and convert it into an array list
+         * of contact's and store it into contacs.
+         */
     }
 }
 
