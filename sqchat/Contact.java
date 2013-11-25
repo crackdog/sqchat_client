0f775375495087030893c09d9ad8a0ed4116a40a
Florian Kahllund <flo.kahllund@googlemail.com>
2013-11-25 11:01:57 +0100

    adding first parts of implementation of Contact.java

--- sqchat/Contact.java
+++ sqchat/Contact.java
@@ -3,9 +3,26 @@ package sqchat;
 import sqchat.*;
 
 public class Contact {
+    
+    private String name, uid;
 
     public Contact() {
+        name = "";
+        uid = "";
+    }
 
+    public void importContact(String contactInfo) {
+        name = "...";
+        uid = "...";
+    }
+    
+    public String print() {
+        String tmp = "";
+
+        tmp += "name: " + name;
+        tmp += "uid: " + uid;
+
+        return tmp;
     }
 }
 
