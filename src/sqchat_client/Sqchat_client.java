92f617c3c1671f2b78ca9eb3462a81467fd0ba4b
Florian Kahllund <flo.kahllund@gmail.com>
2014-02-11 22:00:03 +0100

    src setup

---
+++ src/sqchat_client/Sqchat_client.java
@@ -0,0 +1,15 @@
+package sqchat_client;
+
+/**
+ *
+ * @author flo.kahllund@gmail.com
+ */
+public class Sqchat_client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
